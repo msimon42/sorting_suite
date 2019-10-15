26f9f70439387e099e6d3ef82e8ae3b0fde9a983
msimon42 <msimon@uab.edu>
2019-10-14 21:54:43 -0600

    Add sort method

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,8 +1,23 @@
-class Bubble_sort
-  attr_reader :array, :previous, :current
+class BubbleSort
 
   def initialize(array)
     @array = array
+  end
 
+  def sort
+    n = @array.length
+    while n > 0
+      i = 0
+      (n - 1).times do
+        if @array[i] > @array[i + 1]
+           @array[i],@array[i + 1] = @array[i + 1],@array[i]
+           i += 1
+        else
+           i += 1
+        end
+      end
+      n -= 1
+    end
+    @array
   end
 end
